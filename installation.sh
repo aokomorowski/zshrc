f7e993d4e7fcfe1578b4ce7c3a66ac67a345342b
aokomorowski <43665474+aokomorowski@users.noreply.github.com>
2020-04-02 02:38:00 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -23,20 +23,23 @@ done
 #ohmyzsh
 sudo pacman -S zsh
 chsh -s $(which zsh)
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+trap "sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"" EXIT
 pip3 install thefuck --user
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
+mkdir ~/.local/share/fonts
 mv PowerlineSymbols.otf ~/.local/share/fonts/
 fc-cache -vf ~/.local/share/fonts/
+mkdir ~/.config/fontconfig ~/.config/fontconfig/conf.d
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-wget https://raw.githubusercontent.com/aokomorowski/zshrc/master/.zshrc -o ~/.zshrc
+wget https://raw.githubusercontent.com/aokomorowski/zshrc/master/.zshrc -O ~/.zshrc
 
 # essentials :P
 
 sudo snap install spotify
 sudo snap install docker
 sudo snap install slack --classic
+
