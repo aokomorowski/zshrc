f51772f806325d242c615921a6d1485fff37be59
aokomorowski <43665474+aokomorowski@users.noreply.github.com>
2020-05-22 12:58:42 +0200

    Update installation_mac.sh

--- installation_mac.sh
+++ installation_mac.sh
@@ -23,7 +23,7 @@ done
 #ohmyzsh
 brew install zsh
 brew install wget
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 pip3 install thefuck --user
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
