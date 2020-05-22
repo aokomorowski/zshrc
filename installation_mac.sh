b668e4ada27f72cb21519a2eaa15b5d49b09cdd5
aokomorowski <43665474+aokomorowski@users.noreply.github.com>
2020-05-22 13:15:06 +0200

    Update installation_mac.sh

--- installation_mac.sh
+++ installation_mac.sh
@@ -24,16 +24,12 @@ done
 brew install zsh
 brew install wget
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-pip3 install thefuck --user
+brew install thefuck
 git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
-mkdir ~/.local/share/fonts
-mv PowerlineSymbols.otf ~/.local/share/fonts/
-fc-cache -vf ~/.local/share/fonts/
-mkdir ~/.config/fontconfig ~/.config/fontconfig/conf.d
-mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+mv PowerlineSymbols.otf /Library/Fonts
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
 wget https://raw.githubusercontent.com/aokomorowski/zshrc/master/.zshrc_mac -O ~/.zshrc
 
